d69710b32f93ef7ae993493064f429961b07c31f
Vensah <venkateshdeven14@gmail.com>
2024-10-16 20:32:54 +0800

    trynna use images: { unoptimized: false }

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 const nextConfig = {
     // output: 'export',
 
-    // images: { unoptimized: true }
+    images: { unoptimized: false }
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
