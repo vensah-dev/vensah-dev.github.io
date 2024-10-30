49e4afb019c5e40f53605596c745763fabbd9485
Vensah <venkateshdeven14@gmail.com>
2024-10-30 10:25:25 +0800

    projects page and dynamic routing

---
+++ app/projects/page.js
@@ -0,0 +1,7 @@
+export default function Projects(){
+    return(
+        <div className="pt-[104px] md:pt-[136px]">
+            <h1 className="text-h1 font-bold ">Projects Page</h1>
+        </div>
+    )
+}
