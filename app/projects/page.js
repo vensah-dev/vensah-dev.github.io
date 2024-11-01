e51d43620f6b4bef2db0ff0769b83584563b9b3f
Vensah <venkateshdeven14@gmail.com>
2024-11-01 22:41:29 +0800

    BRO

--- app/projects/page.js
+++ app/projects/page.js
@@ -86,7 +86,7 @@ export default function Projects(){
                 <div className='flex justify-between'>
                     {
                         projects.map((project, index) => (
-                            <ProjectThumbnail path={project.path} title={project.title} image={project.image} tags={project.tags} index={index}/>
+                            <ProjectThumbnail key={index} path={project.path} title={project.title} image={project.image} tags={project.tags} index={index}/>
                         ))
                     }
                 </div>
