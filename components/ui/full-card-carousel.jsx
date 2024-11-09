19c0ccaad2ae417c919be908d0a940aa5baf5119
Vensah <83892875+vensah-dev@users.noreply.github.com>
2024-11-09 19:56:56 +0800

    SOMEONE FORGOT TO CHANGE THE PARENT CONTAINERS ASPECT RATIO… I WONDER WHO HUH?!

--- components/ui/full-card-carousel.jsx
+++ components/ui/full-card-carousel.jsx
@@ -18,7 +18,7 @@ export function FullCardCarousel({content}) {
       <>
 
         <div className="relative w-full border-white10 border-2 rounded-[1rem]">
-            <div className="font-medium flex-grow-0 bg-opacity-0 rounded-[1rem] overflow-clip align-text-top  aspect-[16/8]">
+            <div className="font-medium flex-grow-0 bg-opacity-0 rounded-[1rem] overflow-clip align-text-top  md:aspect-[16/8] aspect-[3/4]">
                 <div className={`flex justify-center w-full self-center bg-tertiary`}>
 
                     <div className='relative w-full md:aspect-[16/8] aspect-[3/4]'>
