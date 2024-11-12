63b909f8e4404edb68c27499c5f1a77e878b17b8
Vensah <venkateshdeven14@gmail.com>
2024-11-12 22:00:53 +0800

    uh so search bar works added a few new features, will soon add tags to the front projects page and then make thsi reactive

--- components/all-projects.js
+++ components/all-projects.js
@@ -1,24 +1,19 @@
 'use client'
-import React, { useState, ChangeEvent } from 'react'
+import React, { useState, useEffect } from 'react'
 
-//icons
+//icons\
+import { IoClose } from "react-icons/io5";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
 //Next stuff
 import Link from 'next/link';
-import { Search } from 'lucide-react';
 import { projects } from '../lib/projects';
 import ProjectThumbnail from './project-thumbnail';
 
 export function AllProjects({}){
 
   const [filteredProjects, setFilteredProjects] = useState(projects);
-  const [inputValue, setValue] = useState("")
-
-  const handleChange = (event) =>{
-      const inputValue = event.target.value;
-      setValue(inputValue);
-  }
+  const [inputValue, setInputValue] = useState("")
 
   const handleSearch = () => {
     const searchTerm = inputValue.toLowerCase();
@@ -27,10 +22,20 @@ export function AllProjects({}){
     setFilteredProjects(filtered);
   }
 
+  const handleChange = (event) =>{
+      const value = event.target.value;
+      setInputValue(value);
+
+      console.log(value);
+  }
+
+  useEffect(() => {
+    handleSearch();
+  }, [inputValue]);
+
   const handleKeyPress = (event) => {
       if (event.key === "Enter") return handleSearch()
   }
-
 
   return (
     <div className="flex-col mx-16">
@@ -40,19 +45,20 @@ export function AllProjects({}){
       <div className='flex flex-row items-center mb-16 bg-secondary rounded-full overflow-clip'>
 
         <div className='px-6'>
-          <FaMagnifyingGlass className='w-5 h-5 fill-white75 child-focus:outline-none child-focus:ring-1 child-focus:ring-white25 '/>
+          <FaMagnifyingGlass className='w-5 h-5 fill-white75'/>
         </div>
 
         <input
             className="block w-full py-4 bg-secondary text-base focus:outline-none focus:ring-0 focus:ring-white25 placeholder:text-white50"
             placeholder={"Search for a project"}
-            // onChange={(e) => {
-            //     handleSearch(e.target.value);
-            // }}
             value={inputValue ?? ""}
             onChange={handleChange}
             onKeyDown={handleKeyPress}
         />
+
+        <div className='px-6'>
+          <IoClose className={`w-5 h-5 fill-white75 ${inputValue == "" ? 'hidden':''}`} onClick={()=>{setInputValue("")}}/>
+        </div>
         
       </div>
 
