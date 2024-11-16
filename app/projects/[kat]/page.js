d25adddec540c2a9713869aa31927ca4822513f2
Vensah <venkateshdeven14@gmail.com>
2024-11-16 20:39:12 +0800

    pls work pls

--- app/projects/[kat]/page.js
+++ app/projects/[kat]/page.js
@@ -7,16 +7,13 @@ import { projects } from '../../../lib/projects';
 import { AllProjects } from '@/components/all-projects';
 import { ProjectsDetailView } from '@/components/project-detail-view';
 
-var paths = [
-    {kat: "all"}
-]
-
-for(items in projects){
-    paths.push({kat: projects.kat});
-}
-
 export const generateStaticParams = async () => {
-   
+
+    const paths = [
+        {kat: "all"},
+        ...projects
+    ]
+    
     return paths.map((p) => ({
       kat: p.kat
     }))
