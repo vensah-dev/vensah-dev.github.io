66b25e877f8fa31f2d400d2633c5c30d7f5d8622
Vensah <venkateshdeven14@gmail.com>
2024-12-09 11:32:52 +0800

    may have fgured out why i have an issue

--- components/ui/card-carousel-content.jsx
+++ components/ui/card-carousel-content.jsx
@@ -9,14 +9,6 @@ import Image from "next/image";
 import { FaArrowLeft } from "react-icons/fa6";
 import { FaArrowRight } from "react-icons/fa6";
 
-function isTouchDevice() {
-  return (
-    "ontouchstart" in window ||
-    navigator.maxTouchPoints > 0 ||
-    navigator.msMaxTouchPoints > 0
-  );
-}
-
 export function CardCarouselContent({ content, position }) {
   const [index, setIndex] = useState(0);
 
@@ -45,11 +37,11 @@ export function CardCarouselContent({ content, position }) {
 
         <div className='lg:absolute lg:bottom-0 flex gap-8 xl:my-8 my-4'>
 
-          <button className={`flex items-center justify-center w-10 h-10 rounded-full bg-white15  ${index === 0 ? 'opacity-50' : isTouchDevice() ? 'hover:opacity-75' : ""} ${showButtons === true ? 'flex' : 'hidden'}`} onClick={() => { if (index > 0) { setIndex(index - 1) } }}>
+          <button className={`flex items-center justify-center w-10 h-10 rounded-full bg-white15  ${index === 0 ? 'opacity-50' : 'md:hover:opacity-75'} ${showButtons === true ? 'flex' : 'hidden'}`} onClick={() => { if (index > 0) { setIndex(index - 1) } }}>
             <FaArrowLeft className='w-6 h-6 fill-white75' />
           </button>
 
-          <button className={`flex items-center justify-center w-10 h-10 rounded-full bg-white15  ${index === content.length - 1 ? 'opacity-50' : isTouchDevice() ? 'hover:opacity-75' : ""} ${showButtons === true ? 'flex' : 'hidden'}`} onClick={() => { if (index < content.length - 1) { setIndex(index + 1) } }}>
+          <button className={`flex items-center justify-center w-10 h-10 rounded-full bg-white15  ${index === content.length - 1 ? 'opacity-50' : 'md:hover:opacity-75'} ${showButtons === true ? 'flex' : 'hidden'}`} onClick={() => { if (index < content.length - 1) { setIndex(index + 1) } }}>
             <FaArrowRight className='w-6 h-6 fill-white75' />
           </button>
 
--- components/ui/full-card-carousel.jsx
+++ components/ui/full-card-carousel.jsx
@@ -9,14 +9,6 @@ import Image from "next/image";
 import { FaArrowLeft } from "react-icons/fa6";
 import { FaArrowRight } from "react-icons/fa6";
 
-function isTouchDevice() {
-    return (
-        "ontouchstart" in window ||
-        navigator.maxTouchPoints > 0 ||
-        navigator.msMaxTouchPoints > 0
-    );
-}
-
 export function FullCardCarousel({ content }) {
     const [index, setIndex] = useState(0);
 
@@ -41,11 +33,11 @@ export function FullCardCarousel({ content }) {
 
                         <div className={`absolute bottom-0 flex md:gap-8 gap-6 md:m-4 m-2 bg-primary p-[3px] backdrop-blur-3xl rounded-full opacity-70 ${showButtons === true ? 'flex' : 'hidden'}`}>
 
-                            <button className={`flex items-center justify-center w-8 h-8 rounded-full bg-white15 backdrop-blur-3xl ${index === 0 ? 'opacity-50' : isTouchDevice() ? 'hover:opacity-75' : ""}`} onClick={() => { if (index > 0) { setIndex(index - 1) } }}>
+                            <button className={`flex items-center justify-center w-8 h-8 rounded-full bg-white15 backdrop-blur-3xl ${index === 0 ? 'opacity-50' : 'md:hover:opacity-75'}`} onClick={() => { if (index > 0) { setIndex(index - 1) } }}>
                                 <FaArrowLeft className='w-6 h-6 fill-white75' />
                             </button>
 
-                            <button className={`flex items-center justify-center w-8 h-8 rounded-full bg-white15 backdrop-blur-3xl ${index === content.length - 1 ? 'opacity-50' : isTouchDevice() ? 'hover:opacity-75' : ""}`} onClick={() => { if (index < content.length - 1) { setIndex(index + 1) } }}>
+                            <button className={`flex items-center justify-center w-8 h-8 rounded-full bg-white15 backdrop-blur-3xl ${index === content.length - 1 ? 'opacity-50' : 'md:hover:opacity-75'}`} onClick={() => { if (index < content.length - 1) { setIndex(index + 1) } }}>
                                 <FaArrowRight className='w-6 h-6 fill-white75' />
                             </button>
 
