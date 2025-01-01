bfb1689c86b6088fa7f9f08ba4b38b1ad234acf5
Vensah <venkateshdeven14@gmail.com>
2025-01-01 11:44:46 +0800

    HAPPY NEW YEAR 🎉 the footer is updated for 2025

--- components/footer.js
+++ components/footer.js
@@ -15,31 +15,31 @@ const Footer = () => {
   const isBrowser = () => typeof window !== 'undefined'; //The approach recommended by Next.js
 
   function scrollToTop() {
-      if (!isBrowser()) return;
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+    if (!isBrowser()) return;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
   return (
     <footer className='bg-secondary '>
-        <div className="flex mx-16 py-6 items-center justify-between">
+      <div className="flex mx-16 py-6 items-center justify-between">
 
-          <button onClick={scrollToTop}>
+        <button onClick={scrollToTop}>
 
-            <div className='flex gap-4 items-center'>
-                <div className='rounded-full w-8 h-8 overflow-hidden'>
-                  <Image
-                    src={profilePic}
-                    alt="vensah-profile-picture"  
-                  />
-                </div>
-
-                <p className='text-white75'>© Venkatesh Devendran 2024.</p>
+          <div className='flex gap-4 items-center'>
+            <div className='rounded-full w-8 h-8 overflow-hidden'>
+              <Image
+                src={profilePic}
+                alt="vensah-profile-picture"
+              />
             </div>
 
-          </button>
+            <p className='text-white75'>© Venkatesh Devendran 2025.</p>
+          </div>
 
-          <ContactGrid footer={true}/>
+        </button>
 
-        </div>
+        <ContactGrid footer={true} />
+
+      </div>
     </footer>
   )
 }
