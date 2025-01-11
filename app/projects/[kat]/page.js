0b7e49d654c181143bfe9c48deca4c67f902a159
Vensah <venkateshdeven14@gmail.com>
2025-01-11 18:57:29 +0800

    inojbubbh

--- app/projects/[kat]/page.js
+++ app/projects/[kat]/page.js
@@ -24,28 +24,28 @@ export async function generateMetadata({ params }) {
     const project = projects.find((p) => p.kat === params.kat)
 
     return {
-        title: `${project ? `${project.title} | Projects ` : "All Projects | Projects"}`,
+        title: `${project ? `${project.title} | Projects ` : "All Projects"}`,
         icons: {
             icon: `${project ? project.icon : "/icons/favicon.ico"}`,
         },
-        // openGraph: {
-        //     title: project ? `${project.title} | Venkatesh` : "All Projects",
-        //     description: project ? project.description.replace("\n", "") : "Check out all the projects I was involved in over the years.",
-        //     images: [
-        //         project ?
-        //             {
-        //                 width: 1080,
-        //                 height: 600,
-        //                 url: 'https://vensah-dev.github.io' + project.image,
-        //             }
-        //             :
-        //             {
-        //                 width: 1900,
-        //                 height: 1900,
-        //                 url: `https://vensah-dev.github.io/icons/open-graph-image.png`,
-        //             }
-        //     ]
-        // },
+        openGraph: {
+            title: project ? `${project.title} | Projects | Venkatesh` : "All Projects | Venkatesh",
+            description: project ? project.description.replace("\n", "") : "Check out all the projects I was involved in over the years.",
+            images: [
+                project ?
+                    {
+                        width: 1080,
+                        height: 600,
+                        url: 'https://vensah-dev.github.io' + project.image,
+                    }
+                    :
+                    {
+                        width: 1900,
+                        height: 1900,
+                        url: `https://vensah-dev.github.io/icons/open-graph-image.png`,
+                    }
+            ]
+        },
     };
 }
 
