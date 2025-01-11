308ea28e1795e69f94a9b71331c8f1a5199cf632
Vensah <venkateshdeven14@gmail.com>
2025-01-11 23:43:38 +0800

    added more details to OG for /projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -22,7 +22,24 @@ import { projects } from '../../lib/projects';
 import ProjectsContent from '@/components/projects-content';
 
 export const metadata = {
-    title: "Projects"
+    title: "Projects",
+    description: "Check out all the projects I was involved in over the years!",
+
+    openGraph: {
+        title: "Projects | Venkatesh",
+        site_name: "Projects | Venkatesh",
+        description: "Check out all the projects I was involved in over the years!",
+        url: "https://vensah-dev.github.io/projects",
+        type: "website",
+        logo: 'https://vensah-dev.github.io/icons/open-graph-image.png',
+        images: [
+            {
+                width: 600,
+                height: 600,
+                url: 'https://vensah-dev.github.io/icons/open-graph-image.png',
+            }
+        ]
+    },
 };
 
 export default function Projects() {
