a4921849650bb2b083758c108d47f8ba07e69ae4
Vensah <venkateshdeven14@gmail.com>
2025-01-11 10:38:23 +0800

    changed tracking for the pycharm text stuf, added a new grpah for lost umbrellas count, new project being added soon

--- components/about-hero.js
+++ components/about-hero.js
@@ -15,64 +15,64 @@ import aboutHero from "@/public/images/about/about-hero.png"
 import { ContactGrid } from './contact-grid';
 import { TextGenerateEffect } from './ui/text-generate-effect';
 
-export function AboutHero(){
+export function AboutHero() {
   return (
 
     <div className="bg-about-hero h-screen bg-cover bg-repeat-x bg-inherit lg:bg-fixed animate-[bg-pan-left_100s_linear_infinite] md:animate-[bg-pan-left_30s_linear_infinite]">
-        <div className="w-screen h-screen flex items-center bg-opacity-0 bg-gradient-to-t from-primary from-20% to-white-opacity-0 to-85% md:bg-gradient-to-r md:from-primary md:from-20% md:to-white-opacity-0 md:to-85% z-100">
+      <div className="w-screen h-screen flex items-center bg-opacity-0 bg-gradient-to-t from-primary from-20% to-white-opacity-0 to-85% md:bg-gradient-to-r md:from-primary md:from-20% md:to-white-opacity-0 md:to-85% z-100">
 
-            <div className='w-full z-0 mx-4 md:mx-16 md:w-2/3 '>
-              <div className=' font-jetBrainsMono font-medium tracking-[-0.117em] flex text-h5 md:text-h4'>
+        <div className='w-full z-0 mx-4 md:mx-16 md:w-2/3 '>
+          <div className=' font-jetBrainsMono font-medium tracking-[-0.05em] flex text-h5 md:text-h4'>
 
-                <TextGenerateEffect
-                  words={"Text"}
-                  className="text-[#8888C1]"
-                  color={"[#8888C1]"}
-                  duration={1}
-                />
-                <TextGenerateEffect
-                  words={"("}
-                  className="text-[#BCBEC3]"
-                  color={"[#BCBEC3]"}
-                  duration={1}
-                />
-                <TextGenerateEffect
-                  words={'“Hello, I’m Venkatesh”'}
-                  className="text-[#79A978]"
-                  color={"[#79A978]"}
+            <TextGenerateEffect
+              words={"Text"}
+              className="text-[#8888C1]"
+              color={"[#8888C1]"}
+              duration={1}
+            />
+            <TextGenerateEffect
+              words={"("}
+              className="text-[#BCBEC3]"
+              color={"[#BCBEC3]"}
+              duration={1}
+            />
+            <TextGenerateEffect
+              words={'“Hello, I’m Venkatesh”'}
+              className="text-[#79A978]"
+              color={"[#79A978]"}
 
-                  duration={1}
-                />
-                <TextGenerateEffect
-                  words={")"}
-                  className="text-[#BCBEC3]"
-                  color={"[#BCBEC3]"}
+              duration={1}
+            />
+            <TextGenerateEffect
+              words={")"}
+              className="text-[#BCBEC3]"
+              color={"[#BCBEC3]"}
 
-                  duration={1}
-                />
-              </div>
+              duration={1}
+            />
+          </div>
 
-              <TextGenerateEffect
-                words="Student, Developer & President"
-                className={"text-h3 md:text-h1 font-bold leading-tight pb-4 md:pb-8 w-full lg:w-[75%] xl:w-[55%]"}
-                color={"white100"}
-                duration={1}
-              />
+          <TextGenerateEffect
+            words="Student, Developer & President"
+            className={"text-h3 md:text-h1 font-bold leading-tight pb-4 md:pb-8 w-full lg:w-[75%] xl:w-[55%]"}
+            color={"white100"}
+            duration={1}
+          />
 
-              <TextGenerateEffect
-                words={"I’m Venkatesh Devendran. A student in Pei Hwa Secondary School, a passionate developer and the president of my Robotics Club"}
-                className={" w-full md:w-3/5 lg:w-1/3 pb-4 animate-blurfadein"}
-                color={"white75"}
-                duration={1}
-                staggerVal={0.05}
-              />
+          <TextGenerateEffect
+            words={"I’m Venkatesh Devendran. A student in Pei Hwa Secondary School, a passionate developer and the president of my Robotics Club"}
+            className={" w-full md:w-3/5 lg:w-1/3 pb-4 animate-blurfadein"}
+            color={"white75"}
+            duration={1}
+            staggerVal={0.05}
+          />
 
-              <ContactGrid about={true}/>
+          <ContactGrid about={true} />
 
-              {/* <PrimaryButton text="My Projects" position={"right"} icon={<IoIosArrowDropdownCircle className='w-6 h-6'/>}/> */}
-            </div>
-
+          {/* <PrimaryButton text="My Projects" position={"right"} icon={<IoIosArrowDropdownCircle className='w-6 h-6'/>}/> */}
         </div>
+
+      </div>
     </div>
   )
 }
--- components/secondary-button.js
+++ components/secondary-button.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export function SecondaryButton({text, position, icon, handleClick, className}){
-  return (
+export function SecondaryButton({ text, position, icon, handleClick, className }) {
+    return (
         <div className={`${className}`}>
             <button className="relative inline-flex overflow-hidden rounded-full" onClick={handleClick}>
-                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_0deg,#679877_0deg,#C5CC66_360deg)]" />
-                <span className="inline-flex h-full w-full transition-all ease-in-out duration-400 items-center justify-center rounded-full active:brightness-75 backdrop-blur-md text-accent">
+                <span className="absolute inset-[-1000%] animate-[spin_2.5s_linear_infinite] bg-[conic-gradient(from_0deg,#679877_0deg,#C5CC66_200deg,#679877_360deg)]" />
+                <span className="inline-flex h-full w-full transition-all ease-in-out duration-400 items-center justify-center rounded-full active:brightness-75 backdrop-blur-xl text-accent">
 
-                    <span className='bg-primary px-6 py-3 m-[2px] gap-4 rounded-full items-center inline-flex justify-center transition-all ease-in-out duration-400'>
+                    <span className='bg-primary px-6 py-3 m-[1.5px] gap-4 rounded-full items-center inline-flex justify-center transition-all ease-in-out duration-400'>
                         {position === "left" && icon}
                         {text}
                         {position === "right" && icon}
