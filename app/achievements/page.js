1b80e9521d12508243b9fe0ff789209d8c84f0ea
Vensah <venkateshdeven14@gmail.com>
2025-04-04 09:27:18 +0800

    yea updated stuff for the neopixel shooter

---
+++ app/achievements/page.js
@@ -0,0 +1,38 @@
+
+//React
+import React from 'react'
+
+//images
+
+//icons
+
+//Next Stuff
+import Image from "next/image";
+import Link from "next/link";
+
+
+//components
+import { SecondaryButton } from '@/components/secondary-button'
+
+import FeaturedProject from '@/components/featured-project';
+import ProjectThumbnail from '@/components/project-thumbnail';
+
+//Data
+import { projects } from '../../lib/projects';
+import ProjectsContent from '@/components/projects-content';
+
+export const metadata = {
+    title: "Achievements"
+};
+
+export default function Testimonials() {
+
+
+    return (
+        <>
+            <div className='pt-[136px]'>
+                
+            </div>
+        </>
+    )
+}
